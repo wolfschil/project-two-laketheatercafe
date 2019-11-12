cd8f7abe9e57a410bcaeaecd077928be40319ecc
wolfschil <56000960+wolfschil@users.noreply.github.com>
2019-11-12 11:30:05 -0800

    little head way

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,28 @@
 // your script file for adding your own jquery
-$(function() {
-// Your Code from here on down. Don't delete that line above!
+//  $(function( myFunction) {
+// Your Code from here on down. Don't delete that line above! (haivng the line above made it not work)
   
 
 
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
 
 
 
@@ -15,6 +34,5 @@ $(function() {
 
 
 
-
-// End of Your Code . Don't delete that line below!!
-});
+// End of Your Code . Don't delete that line below!! (haivng the line bellow made it not work)
+//   });
