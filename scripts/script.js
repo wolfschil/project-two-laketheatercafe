338b2b9f717ba32435ed9210aa86973bb0167905
wolfschil <56000960+wolfschil@users.noreply.github.com>
2019-11-12 19:07:14 -0800

    getting confusing

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,46 @@
 //  $(function( myFunction) {
 // Your Code from here on down. Don't delete that line above! (haivng the line above made it not work)
   
+$('.img-parallax').each(function(){
+  var img = $(this);
+  var imgParent = $(this).parent();
+  function parallaxImg () {
+    var speed = img.data('speed');
+    var imgY = imgParent.offset().top;
+    var winY = $(this).scrollTop();
+    var winH = $(this).height();
+    var parentH = imgParent.innerHeight();
+
+
+    // The next pixel to show on screen      
+    var winBottom = winY + winH;
+
+    // If block is shown on screen
+    if (winBottom > imgY && winY < imgY + parentH) {
+      // Number of pixels shown after block appear
+      var imgBottom = ((winBottom - imgY) * speed);
+      // Max number of pixels until block disappear
+      var imgTop = winH + parentH;
+      // Porcentage between start showing until disappearing
+      var imgPercent = ((imgBottom / imgTop) * 100) + (50 - (speed * 50));
+    }
+    img.css({
+      top: imgPercent + '%',
+      transform: 'translate(-50%, -' + imgPercent + '%)'
+    });
+  }
+  $(document).on({
+    scroll: function () {
+      parallaxImg();
+    }, ready: function () {
+      parallaxImg();
+    }
+  });
+
+
+
+
+});
 
 
 /* When the user clicks on the button, 
@@ -23,7 +63,6 @@ window.onclick = function(event) {
     }
   }
 }
-
 
 
 
